25eb99fa146faeb9b27cf3f69fdd9cb2367d277f
Alex Baranov <abaranov@ergo.inc>
2020-08-22 21:32:58 -0400

    registration / login with subscription activation complete

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -64,8 +65,11 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        // dd($data['is_subscribed']);
         return User::create([
-            'name' => $data['name'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'is_subscribed' => $data['is_subscribed'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'first_name' => 'George',
             'last_name' => 'Washington',
+            'is_subscribed' => '1',
             'email' => 'GDubz@gmail.com',
             'password' => bcrypt('password'),
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
@@ -25,6 +26,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'first_name' => 'Alex',
             'last_name' => 'Baranov',
+            'is_subscribed' => '1',
             'email' => 'alexbaranov43@gmail.com',
             'password' => bcrypt('password'),
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
@@ -34,6 +36,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'first_name' => 'Joe',
             'last_name' => 'Schmoe',
+            'is_subscribed' => '1',
             'email' => 'JoeSchmoe@gmail.com',
             'password' => bcrypt('password'),
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
@@ -43,6 +46,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'first_name' => 'Tyler',
             'last_name' => 'Durden',
+            'is_subscribed' => '1',
             'email' => 'TDurdenFC@gmail.com',
             'password' => bcrypt('password'),
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
@@ -52,6 +56,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'first_name' => 'Random',
             'last_name' => 'Guy',
+            'is_subscribed' => '1',
             'email' => 'RGuy@gmail.com',
             'password' => bcrypt('password'),
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
